06dd1770eafef372c5d812b37d2c380d428d445b
razvan <razvan.becheru@conicdesign.ro>
2019-03-18 20:11:26 +0200

    Good enough layout for messages

--- site/src/components/Messages.vue
+++ site/src/components/Messages.vue
@@ -1,10 +1,9 @@
 <template>
 
-    <v-layout row wrap >
+    <v-layout row wrap>
 
 
-
-        <v-flex xs12>
+        <v-flex xs8 offset-md2>
             <v-flex xs12 class="pa-0 pb-3">
                 <v-card>
                     <v-flex>
@@ -71,19 +70,20 @@
                                     <v-date-picker v-model="endDatePicker.date" no-title scrollable>
                                         <v-spacer></v-spacer>
                                         <v-btn flat color="primary" @click="endDatePicker.menu = false">Cancel</v-btn>
-                                        <v-btn flat color="primary" @click="$refs.endDatePicker.save(endDatePicker.date)">OK
+                                        <v-btn flat color="primary"
+                                               @click="$refs.endDatePicker.save(endDatePicker.date)">OK
                                         </v-btn>
                                     </v-date-picker>
                                 </v-menu>
                             </v-flex>
-                            <v-flex md2 d-flex >
+                            <v-flex md2 d-flex>
                                 <v-checkbox
                                         label="Only valid"
                                         v-model="onlyValidCheckbox"
                                 ></v-checkbox>
                             </v-flex>
                             <v-flex md3 d-flex>
-                                <v-btn depressed  color="primary lighten-1" @click="onDownloadPressed" class="pr-3">
+                                <v-btn depressed color="primary lighten-1" @click="onDownloadPressed" class="pr-3">
                                     <v-icon dark class="pr-3">cloud_download</v-icon>
                                     Download
                                 </v-btn>
@@ -96,54 +96,62 @@
             </v-flex>
             <v-card class="pa-3 fill-height">
                 <v-layout row wrap>
-                    <v-flex xs4>
+                    <v-flex xs12 fill-height>
                         <p class="title pl-3">Messages </p>
-                        <v-divider></v-divider>
+
 
                         <v-list>
-                            <v-list-tile v-for="(message,index) in messages" :key="index" @click="showMessage(message)" v-bind:class="{'v-list__tile--highlighted':message.highlight}"  >
+                            <v-card v-for="(message,index) in messages" :key="index" flat class="my-2"  style="border:1px solid rgb(230, 230, 230)">
+                                <v-list-tile @click="highlightMessage(message)"
+                                             v-bind:class="{'v-list__tile--highlighted':message.highlight}">
 
 
-                                <v-list-tile-content>
-                                    <v-list-tile-title v-text="message.card.title"></v-list-tile-title>
-                                    <v-list-tile-sub-title>{{message.card.subtitle}}</v-list-tile-sub-title>
+                                    <v-list-tile-content>
+                                        <v-list-tile-title v-text="message.card.title"></v-list-tile-title>
+                                        <v-list-tile-sub-title>{{message.card.subtitle}}</v-list-tile-sub-title>
 
-                                </v-list-tile-content>
-                                <v-list-tile-action>
-                                    <v-list-tile-action-text>{{message.card.time}}</v-list-tile-action-text>
-                                </v-list-tile-action>
+                                    </v-list-tile-content>
+                                    <v-list-tile-action>
+                                        <v-list-tile-action-text>{{message.card.time}}</v-list-tile-action-text>
+                                    </v-list-tile-action>
+                                    <v-icon class="grey--text">{{message.highlight?'expand_less':'expand_more'}}</v-icon>
 
 
-                            </v-list-tile>
+
+                                </v-list-tile>
+
+                                <v-card class="pb-3 pt-3 mb-4" flat v-if="message.highlight" >
+
+                                    <v-flex xs12class="pa-3">
+                                        <v-layout row v-for="(property,index) in message.properties" wrap class="py-1">
+
+                                            <v-flex xs2 class="py-0">
+                                                <p class="text-capitalize my-0 grey--text ">{{property}}:</p>
+                                            </v-flex>
+                                            <v-flex xs10 class="py-0 ">
+                                                <p class="align-end  my-0  subheading">
+                                                    {{message.payload[property]}}</p>
+                                            </v-flex>
+                                            <v-flex xs112 class="py-0" v-if="index<message.properties.length-1">
+                                                <v-divider></v-divider>
+                                            </v-flex>
+
+                                        </v-layout>
+
+
+                                    </v-flex>
+
+
+                                </v-card>
+
+
+                            </v-card>
                         </v-list>
+
+
                     </v-flex>
 
 
-                    <v-divider vertical></v-divider>
-
-                    <v-flex xs7>
-                        <v-list>
-                            <v-list-tile
-                                    v-for="item in items"
-                                    :key="item.title"
-                                    avatar
-                                    @click="alert('Test')"
-                            >
-
-
-                                <v-list-tile-content>
-                                    <v-list-tile-title v-text="item.title"></v-list-tile-title>
-                                    <v-list-tile-sub-title>sub title</v-list-tile-sub-title>
-
-                                </v-list-tile-content>
-                                <v-list-tile-action>
-                                    <v-list-tile-action-text>Test</v-list-tile-action-text>
-                                </v-list-tile-action>
-
-
-                            </v-list-tile>
-                        </v-list>
-                    </v-flex>
                 </v-layout>
             </v-card>
         </v-flex>
@@ -163,8 +171,8 @@
         name: "Messages",
         data: function () {
             return {
-                messages:[],
-                currentMessage:null,
+                messages: [],
+                currentMessage: null,
                 startDatePicker: {
                     date: moment().subtract(1, 'months').format("YYYY-MM-DD"),
                     menu: false,
@@ -223,30 +231,34 @@
 
 
             },
-            showMessage:function(message){
-                if(this.currentMessage!==null)
-                     this.currentMessage.highlight = false;
-                this.currentMessage = message;
-                this.currentMessage.highlight = true;
+            highlightMessage: function (message) {
 
+                if(message.highlight){
+                    message.highlight=false;
+                    return;
+                }
+                this.messages.forEach((message) => {
+                    message.highlight = false;
+                });
+                message.highlight = !message.highlight;
             }
         },
-        watch:{
-            'currentWebsite': function(){
-                let that =this;
+        watch: {
+            'currentWebsite': function () {
+                let that = this;
                 api.pullMessages(this.currentWebsite.key, this.selectedForm, 0, 99999999999999, this.onlyValidCheckbox, function (res) {
                     console.log(res);
 
 
-
-                    that.messages = res.map((message)=>{
+                    that.messages = res.map((message) => {
                         let payload = message.payload;
                         message.highlight = false;
                         message.card = {
                             title: payload[Object.keys(payload)[0]],
                             subtitle: payload[Object.keys(payload)[1]],
-                            time: moment.unix(message.timestamp/1000).format("LL")
+                            time: moment.unix(message.timestamp / 1000).format("LL")
                         };
+                        message.properties = Object.keys(message.payload);
                         return message;
                     });
                 });
